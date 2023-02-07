9d8ec813ec3b12e260cb513d018433df88c60df7
Spirevipp <bjornar.helgeland@hotmail.com>
2023-02-07 01:19:54 +0100

    initial commit

---
+++ data.lua
@@ -0,0 +1,35 @@
+--data.lua
+
+-- subgroups
+data:extend({
+	{
+		type = "item-subgroup",
+		name = "miniloaders",
+		group = "logistics",
+		order = "bbb",
+	},
+	{
+		type = "item-subgroup",
+		name = "miniloaders-filtered",
+		group = "logistics",
+		order = "bbb",
+	},
+	{
+		type = "item-subgroup",
+		name = "ducts",
+		group = "logistics",
+		order = "d",
+	},
+	{
+		type = "item-subgroup",
+		name = "manifolds",
+		group = "logistics",
+		order = "dd",
+	},
+	{
+		type = "item-subgroup",
+		name = "waterfill",
+		group = "dectorio",
+		order = "dd",
+	},
+})
